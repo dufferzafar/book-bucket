24aaa3f19b4423547ee0e6fd825a4bd3c7746cf0
Shadab Zafar <dufferzafar0@gmail.com>
2014-09-17 02:24:31 +0530

    Script can now fetch posts of all the friends
    Friend IDs are not written to disk now.
    
    Only first 5 pages of posts are fetched.

--- fetch.py
+++ fetch.py
@@ -1,41 +1,47 @@
 import os
 import json
-
 from facepy import GraphAPI
+
+# Function added to avoid Decimal() serialization issues
+# http://stackoverflow.com/a/16957370/2043048
+import decimal
+def decimal_default(obj):
+    if isinstance(obj, decimal.Decimal):
+        return float(obj)
+    raise TypeError
+
 access_token = ""
 graph = GraphAPI(access_token)
+me = graph.get("me")
 
-# Create the output folder if it doesn't already exist
-folder_posts = 'data/posts'
-folder_friends = 'data/friends'
-
-if not os.path.isdir(folder_posts):
-    os.makedirs(folder_posts)
-if not os.path.isdir(folder_friends):
-    os.makedirs(folder_friends)
+# The folders
+folder = 'data/posts'
 
 # Gather friends
-count = 1
-friend_pages = graph.get("me/friends", page=True)
-for friend_page in friend_pages:
-
-    friend_file = os.path.join(folder_friends, str(count) + ".json")
-    with open(friend_file, 'w') as f:
-        json.dump(friend_page, f, indent=1)
-
-    count = count + 1
-    if count > 5:
-        break
+friend_list = [me['id']]
+pages = graph.get("me/friends", page=True)
+for page in pages:
+    for friend in page['data']:
+        friend_list.append(friend['id'])
 
 # Gather posts
-count = 1
-posts_pages = graph.get('me/posts', page=True)
-for posts_page in posts_pages:
+for friend_id in friend_list:
 
-    post_file = os.path.join(folder_posts, str(count) + ".json")
-    with open(post_file, 'w') as f:
-        json.dump(posts_page, f, indent=1)
+    print("Fetching: " + friend_id)
 
-    count = count + 1
-    if count > 5:
-        break
+    posts_folder = os.path.join(folder, friend_id)
+    if not os.path.isdir(posts_folder):
+        os.makedirs(posts_folder)
+
+    count = 1
+    pages = graph.get(str(friend_id) + '/posts', page=True)
+
+    for page in pages:
+
+        post = os.path.join(posts_folder, str(count) + ".json")
+        with open(post, 'w') as f:
+            json.dump(page, f, indent=1, default=decimal_default)
+
+        count = count + 1
+        if count > 5:
+            break
